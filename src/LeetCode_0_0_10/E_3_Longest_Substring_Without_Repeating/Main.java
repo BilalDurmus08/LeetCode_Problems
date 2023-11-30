964e82cfe0de43d5a2f4b256b5fd08c704d6d6e4
Bilal Durmuş <bilaldurmus08@gmail.com>
2023-11-30 22:43:04 +0300

    third

---
+++ src/LeetCode_0_0_10/E_3_Longest_Substring_Without_Repeating/Main.java
@@ -0,0 +1,12 @@
+package LeetCode_0_0_10.E_3_Longest_Substring_Without_Repeating;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution2 solution2 = new Solution2();
+        String s = "asdfgasdhj";
+        String s1 = "asdasdafdg";
+        System.out.println(solution2.lengthOfLongestSubstring(s1));
+    }
+
+}
---
+++ src/LeetCode_0_0_10/E_3_Longest_Substring_Without_Repeating/Solution2.java
@@ -0,0 +1,36 @@
+package LeetCode_0_0_10.E_3_Longest_Substring_Without_Repeating;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution2 {
+    public int lengthOfLongestSubstring(String s) {
+        char[] charArray = s.toCharArray();
+        Set<Character> mySet;
+        int k;
+        int j;
+        int max = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            mySet = new HashSet<>();
+            k = 0;
+            j = i;
+            while (true) {
+                if (mySet.contains(charArray[j])) {
+                    break;
+                }
+                mySet.add(charArray[j]);
+                k++;
+                j++;
+                if (j == charArray.length){
+                    break;
+                }
+            }
+            if (max < k) {
+                max = k;
+            }
+        }
+        return max ;
+    }
+}
